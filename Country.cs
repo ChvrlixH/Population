185bb4a8164d8ce4220c858f4c31164d08791301
ChvrlixH <ehedzade.huseyn@gmail.com>
2022-12-18 23:44:46 +0400

    Country and City Population

---
+++ Country.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population
+{
+    internal class Country
+    {
+        public string Name;
+        public int TotalPopulation;
+        City[] cities = new City[0];
+
+        public void addCity (City Cities)
+        {
+            Array.Resize(ref cities, cities.Length + 1);
+            cities[cities.Length - 1] = Cities;
+        }
+
+        public Country(string name, int totalPopulation)
+        {
+            Name = name;
+            TotalPopulation = totalPopulation;
+        }
+
+        public void FindAllByPopulation(int min, int max)
+        {
+            Console.WriteLine($"{min} ve {max} araligindaki insanlara sahib seherler: ");
+            foreach (City item in cities)
+            {
+                if (min <= item.Population && item.Population <= max)
+                {
+                    Console.WriteLine(item);
+                }   
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Population
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country1 = new Country("Norway", 27000000);
+            City city1 = new City("Oslo",3000000);
+            City city2 = new City("Bergen",1000000);
+            City city3 = new City("Korgen",70000);
+            City city4 = new City("Drammen",400000);
+            City city5 = new City("Eidjord",600000);
+
+            Country country2 = new Country("Holland", 32000000);
+            City city6 = new City("Amsterdam",7000000);
+            City city7 = new City("Rotterdam",2000000);
+            City city8 = new City("Eindhoven",1000000);
+            City city9 = new City("Lahey",800000);
+            City city10 = new City("Utrecht",280000);
+            
+            City[] cities = { city1, city2, city3, city4, city5, city6, city7, city8, city9, city10 };
+            Country allCity = new($"{cities}",500000);
+            allCity.FindAllByPopulation(300000, 1000000);
+        }
+    }
+}
